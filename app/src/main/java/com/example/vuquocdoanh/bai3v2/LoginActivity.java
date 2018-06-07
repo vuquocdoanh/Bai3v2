781ddbc9d9579fb26bb4c81eedd1e6b4e505e024
vuquocdoanh <doanhgt95@gmail.com>
2018-06-07 16:45:24 +0700

    commit changed

---
+++ app/src/main/java/com/example/vuquocdoanh/bai3v2/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.example.vuquocdoanh.bai3v2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vu Quoc Doanh on 6/7/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+}
---
+++ app/src/main/java/com/example/vuquocdoanh/bai3v2/Student.java
@@ -0,0 +1,8 @@
+package com.example.vuquocdoanh.bai3v2;
+
+/**
+ * Created by Vu Quoc Doanh on 6/7/2018.
+ */
+
+public class Student {
+}
